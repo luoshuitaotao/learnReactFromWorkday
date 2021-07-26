4e180a8cb2b264ab15b5eda458f646e3ed2aafa0
luoshuitaotao <jcui@unomaha.edu>
2021-07-26 11:38:15 -0500

    Learn the basic knowledge about React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import restaurant from "./restaurant.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Jing's" />
+      <Main adjective="delicious" dishes={dishObjects}/>
+      <Footer year= { new Date().getFullYear()} />
+
     </div>
-  );
-}
+  );}
+  function Header(props){
+    return(
+      <div>{ props.name} kitchen</div>
+    )
+  };
+
+  function Main(props){
+    return(
+      <section>
+        <p>We serve the most { props.abjective } food around.</p>
+      <img src={restaurant} height={300} alt="tables and chairs at the restaurant"/>
+      <ul style={ {textAlign:"left"}}>
+        { props.dishes.map((dish)=>(
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+      </section>
+    )
+  };
+  function Footer(props){
+    return(
+      <div>Copyright {props.year }</div>
+    )
+    
+  };
+  const dishes =[
+    "Macaroni and Cheese",
+    "Lanzhou Noodles", 
+    "Fish Soup"
+  ]
+  const dishObjects = dishes.map((dish,i)=>({id: i, title:dish}));
+
 
 export default App;
