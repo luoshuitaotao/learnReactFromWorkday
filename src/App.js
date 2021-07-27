ce42b5443f1f999bffebfa829bee68fba0138ffa
luoshuitaotao <jcui@unomaha.edu>
2021-07-27 09:06:07 -0500

    learn call API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
+//https://api.github.com/users/luoshuitaotao
 
-function App() {
-  const [checked, toggle] = useReducer(
-    checked => !checked,
-    false
-    );
+function App({login}) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError]= useState(null);
   
-  return(
-    <>
-      <input type="checkbox" 
-      value= {checked} 
-      onChange={ toggle } /> 
-      <p>{checked ? "checked" : "not checked"}</p>
-    </>
-  );
-}
+  useEffect(() => {
+    if (!login) return;
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData)
+      .then(()=> setLoading(false))
+      .catch(setError);
+  }, [login]);
 
+  if (loading) return <h1>Loading...</h1>;
+  if (error)
+    return <pre>{JSON.stringify(error, null,2)}</pre>;
+  if (!data) return null;  
 
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.location}</p>
+      <img alt= {data.login} src={data.avatar_url}/>
+    </div>);
+  }
 
 export default App;
